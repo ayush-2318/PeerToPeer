754bb0466583b874a81d3498dafa697cb37ec27a
Ayush Kumar <golutpr@gmail.com>
2025-06-27 10:28:22 +0530

    initial commit

---
+++ src/main/java/p2p/service/FileSharer.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.util.HashMap;
+
+public class FileSharer {
+    private HashMap<Integer,String> avialableFiles;
+
+    public FileSharer(){
+        avialableFiles=new HashMap<Integer, String>();
+    }
+}
